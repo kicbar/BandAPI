d8deb62184c155d88b049a60c874dcc33651d0ea
kicbar <bartlomiejkicior@gmail.com>
2021-01-15 00:52:40 +0100

    implement repository

---
+++ Services/BandAlbumRepository.cs
@@ -0,0 +1,132 @@
+﻿using BandAPI.Data;
+using BandAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Services
+{
+    public class BandAlbumRepository : IBandAlbumRepository
+    {
+        private readonly BandAlbumContext _bandAlbumContext;
+
+        public BandAlbumRepository(BandAlbumContext bandAlbumContext)
+        {
+            _bandAlbumContext = bandAlbumContext ?? throw new ArgumentNullException(nameof(bandAlbumContext));
+        }
+
+        public void AddAlbum(Guid bandId, Album album)
+        {
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            if (album == null)
+                throw new ArgumentNullException(nameof(album));
+
+            album.BandId = bandId;
+
+            _bandAlbumContext.Albums.Add(album);
+        }
+
+        public void AddBand(Guid bandId, Band band)
+        {
+            if (band == null)
+                throw new ArgumentNullException(nameof(band));
+
+            _bandAlbumContext.Bands.Add(band);
+        }
+
+        public bool AlbumExists(Guid albumId)
+        {
+            if (albumId == Guid.Empty)
+                throw new ArgumentNullException(nameof(albumId));
+
+            return _bandAlbumContext.Albums.Any(a => a.Id == albumId);
+        }
+
+        public bool BandExists(Guid bandId)
+        {
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _bandAlbumContext.Bands.Any(a => a.Id == bandId);
+        }
+
+        public void DeleteAlbum(Album album)
+        {
+            if (album == null)
+                throw new ArgumentNullException(nameof(album));
+
+            _bandAlbumContext.Albums.Remove(album);
+        }
+
+        public void DeleteBand(Band band)
+        {
+            if (band == null)
+                throw new ArgumentNullException(nameof(band));
+
+            _bandAlbumContext.Bands.Remove(band);
+        }
+
+        public Album GetAlbum(Guid bandId, Guid albumId)
+        {
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            if (albumId == null)
+                throw new ArgumentNullException(nameof(albumId));
+
+            return _bandAlbumContext.Albums.Where(a => a.BandId == bandId && a.Id == albumId)
+                .FirstOrDefault();
+        }
+
+        public IEnumerable<Album> GetAlbums(Guid bandId)
+        {
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _bandAlbumContext.Albums.Where(a => a.BandId == bandId)
+                .OrderBy(a => a.Title)
+                .ToList();
+        }
+
+        public Band GetBand(Guid bandId)
+        {
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _bandAlbumContext.Bands.FirstOrDefault(b => b.Id == bandId);
+        }
+
+        public IEnumerable<Band> GetBands()
+        {
+            return _bandAlbumContext.Bands.ToList();
+        }
+
+        public IEnumerable<Band> GetBands(IEnumerable<Guid> bandsIds)
+        {
+            if (bandsIds == null)
+                throw new ArgumentNullException(nameof(bandsIds));
+
+            return _bandAlbumContext.Bands.Where(b => bandsIds.Contains(b.Id))
+                .OrderBy(b => b.Name)
+                .ToList();
+        }
+
+        public bool Save()
+        {
+            return (_bandAlbumContext.SaveChanges() >= 0);
+        }
+
+        public void UpdateAlbum(Guid bandId, Album album)
+        {
+            //not implemented
+        }
+
+        public void UpdateBand(Guid bandId, Band band)
+        {
+            //not implemented
+        }
+    }
+}
--- Services/IBandAlbumRepository.cs
+++ Services/IBandAlbumRepository.cs
@@ -1,8 +1,6 @@
 ﻿using BandAPI.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BandAPI.Services
 {
@@ -12,17 +10,17 @@ namespace BandAPI.Services
         Album GetAlbum(Guid bandId, Guid albumId);
         void AddAlbum(Guid bandId, Album album);
         void UpdateAlbum(Guid bandId, Album album);
-        void DeleteAlbum(Guid bandId, Album album);
+        void DeleteAlbum(Album album);
 
         IEnumerable<Band> GetBands();
-        IEnumerable<Band> GetBands(IEnumerable<Guid> bandsId);
+        IEnumerable<Band> GetBands(IEnumerable<Guid> bandsIds);
         Band GetBand(Guid bandId);
         void AddBand(Guid bandId, Band band);
         void UpdateBand(Guid bandId, Band band);
-        void DeleteBand(Guid bandId, Band band);
+        void DeleteBand(Band band);
 
         bool BandExists(Guid bandId);
-        bool AlbumExists(Album albumId);
+        bool AlbumExists(Guid albumId);
         bool Save();
     }
 }
