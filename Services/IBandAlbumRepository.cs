6ad24b50dfc454ee9decc752c994210229d3db48
kicbar <bartlomiejkicior@gmail.com>
2021-01-15 00:16:08 +0100

    add repository interface

---
+++ Services/IBandAlbumRepository.cs
@@ -0,0 +1,28 @@
+﻿using BandAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Services
+{
+    public interface IBandAlbumRepository
+    {
+        IEnumerable<Album> GetAlbums(Guid bandId);
+        Album GetAlbum(Guid bandId, Guid albumId);
+        void AddAlbum(Guid bandId, Album album);
+        void UpdateAlbum(Guid bandId, Album album);
+        void DeleteAlbum(Guid bandId, Album album);
+
+        IEnumerable<Band> GetBands();
+        IEnumerable<Band> GetBands(IEnumerable<Guid> bandsId);
+        Band GetBand(Guid bandId);
+        void AddBand(Guid bandId, Band band);
+        void UpdateBand(Guid bandId, Band band);
+        void DeleteBand(Guid bandId, Band band);
+
+        bool BandExists(Guid bandId);
+        bool AlbumExists(Album albumId);
+        bool Save();
+    }
+}
