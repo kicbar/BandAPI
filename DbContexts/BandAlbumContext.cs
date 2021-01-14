c6bfd48db053fe620dd6370a2ab4b8ebdc532bda
kicbar <bartlomiejkicior@gmail.com>
2021-01-14 23:45:07 +0100

    add db context

---
+++ DbContexts/BandAlbumContext.cs
@@ -0,0 +1,15 @@
+﻿using BandAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BandAPI.DbContexts
+{
+    public class BandAlbumContext : DbContext
+    {
+        public BandAlbumContext(DbContextOptions<BandAlbumContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Band> Bands { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
