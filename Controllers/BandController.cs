2e02a470a2386ff32636b2c2ddb528e20db0fc1a
kicbar <bartlomiejkicior@gmail.com>
2021-01-15 21:32:18 +0100

    add band controller and first get method

---
+++ Controllers/BandController.cs
@@ -0,0 +1,32 @@
+﻿using BandAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Controllers
+{
+    [ApiController]
+    [Route("api/bands")]
+    public class BandController : ControllerBase
+    {
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+
+        public BandController(IBandAlbumRepository bandAlbumRepository)
+        {
+            _bandAlbumRepository = bandAlbumRepository ??
+                throw new ArgumentNullException(nameof(bandAlbumRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetBands()
+        {
+            var bands = _bandAlbumRepository.GetBands();
+
+            return new JsonResult(bands);
+
+        }
+
+    }
+}
