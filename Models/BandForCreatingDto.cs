18089b229170937665370fa217c1fe6d1e4cf4a5
kicbar <bartlomiejkicior@gmail.com>
2021-01-18 23:44:43 +0100

    add dto for create band

---
+++ Models/BandForCreatingDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BandAPI.Models
+{
+    public class BandForCreatingDto
+    {
+        public string Name { get; set; }
+        public DateTime Founded { get; set; }
+        public string MainGenre { get; set; }
+    }
+}
