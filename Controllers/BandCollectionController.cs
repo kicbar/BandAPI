00fe579a6b0d6cf7ce0c8fbdc0a918a6bc34423c
b.kicior <bk.7@o2.pl>
2021-01-19 16:22:46 +0100

    add controller for collection of band

---
+++ Controllers/BandCollectionController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using BandAPI.Models;
+using BandAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Controllers
+{
+    [ApiController]
+    [Route("api/bandcollection")]
+    public class BandCollectionController : ControllerBase
+    {
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+        private readonly IMapper _mapper;
+
+        public BandCollectionController(IBandAlbumRepository bandAlbumRepository, IMapper mapper)
+        {
+            _bandAlbumRepository = bandAlbumRepository ??
+                throw new ArgumentNullException(nameof(bandAlbumRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<BandDto>> CreateBandCollection([FromBody] IEnumerable<BandForCreatingDto> bandsDto)
+        {
+            
+            var bands = _mapper.Map<IEnumerable<Entities.Band>>(bandsDto);
+
+            foreach (var band in bands)
+            {
+                _bandAlbumRepository.AddBand(band);
+            }
+            _bandAlbumRepository.Save();
+
+            return Ok();
+        }
+
+
+
+    }
+}
