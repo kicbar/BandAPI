e91ecb3b63b6b22be67c3d94077e4833742b3c47
kicbar <bartlomiejkicior@gmail.com>
2021-01-19 22:49:14 +0100

    add validation in album

--- Models/AlbumForCreatingDto.cs
+++ Models/AlbumForCreatingDto.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BandAPI.Models
 {
-    public class AlbumForCreatingDto
+    public class AlbumForCreatingDto : IValidatableObject
     {
         [Required]
         [MaxLength(200)]
         public string Title { get; set; }
         [MaxLength(400)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            { 
+                yield return new ValidationResult("The title and description need to be different.", new[] {"AlbumForCreatingDto"});
+            }
+        }
     }
 }
