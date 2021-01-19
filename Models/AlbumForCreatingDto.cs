1686f03d1a998eb8aeae3a3cbc5c5f58f86d63f8
kicbar <bartlomiejkicior@gmail.com>
2021-01-19 23:16:47 +0100

    modify attribute

--- Models/AlbumForCreatingDto.cs
+++ Models/AlbumForCreatingDto.cs
@@ -5,21 +5,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BandAPI.Models
 {
-    [TitleAndDescriptionAttribute]
-    public class AlbumForCreatingDto //: IValidatableObject
+    [TitleAndDescription(ErrorMessage = "Title must Be Different From Description")]
+    public class AlbumForCreatingDto
     {
-        [Required]
-        [MaxLength(200)]
+        [Required(ErrorMessage = "Title needs to be field in")]
+        [MaxLength(200, ErrorMessage = "Title needs to be up to 200 characters")]
         public string Title { get; set; }
-        [MaxLength(400)]
+        [MaxLength(400, ErrorMessage = "Title needs to be up to 200 characters")]
         public string Description { get; set; }
-
-        /*public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            { 
-                yield return new ValidationResult("The title and description need to be different.", new[] {"AlbumForCreatingDto"});
-            }
-        }*/
     }
 }
