49a527dd54ef9ec7b30236cece56ec33c66d18b7
kicbar <bartlomiejkicior@gmail.com>
2021-04-10 00:20:15 +0200

    extend ResourceParameters to pagination

--- Helpers/BandResourceParameters.cs
+++ Helpers/BandResourceParameters.cs
@@ -4,6 +4,15 @@
     {
         public string MainGenre { get; set; }
         public string SearchQuery { get; set; }
-        
+
+        const int maxPageSize = 3;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 2;
+        public int PageSize 
+        { 
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value; 
+        }
+
     }
 }
